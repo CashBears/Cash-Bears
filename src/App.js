0b6ad37ae89c26008b5902fa8dc3b5da33489809
CBears <dev@cashbears.online>
2022-06-25 19:20:35 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -327,7 +327,6 @@ function App() {
                     </s.Container>
             
                 )}
-              </>
             )}
             <s.SpacerMedium />
           </s.Container>
